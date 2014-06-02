6dffc9a518483d138c1094ea1e4f6635b37b8899
canyon289 <ravinsdrive@gmail.com>
2014-06-01 19:33:24 -0500

    Completed Part 1

--- Programming Assignment 3/best.r
+++ Programming Assignment 3/best.r
@@ -1,16 +1,30 @@
 #Set working directory and load file
 setwd("C:/Users/carbon/Dropbox/Github/DataScience/Programming Assignment 3")
 
-#Read table
-data = read.table("outcome-of-care-measures.csv", sep =",", header = T,na.strings="Not Available")
-
 #Set State and condition
-state = "MD"
-condition = ""
-
-#Create Sub Table
-sub = droplevels(data[data$State == state,])
-
-z = tapply(sub$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack, sub$Hospital.Name, FUN = mean, na.rm=T)
-z = sort(z)
-z[1]
+best = function(state, outcome){
+    #Read Data
+    data = read.table("outcome-of-care-measures.csv", sep =",", header = T,na.strings="Not Available")
+    
+    if(!(state %in% data$State)){
+        stop("invalid state")
+    }
+    conditions = c("heart attack", "heart failure", "pneumonia")
+    if(!(outcome %in% conditions)){
+        return("invalid outcome")
+    }
+    else{
+    #Make dictionary of values
+    l = list("heart attack" = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack",
+             "heart failure" = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure",
+             "pneumonia" = "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
+    
+    
+    #Create Sub Table
+    sub = droplevels(data[data$State == state,])
+    
+    z = tapply(sub[[l[[outcome]]]], sub[["Hospital.Name"]], FUN = mean, na.rm=T)
+    z = sort(z)
+    #rownames(z)[1]
+    }
+}
