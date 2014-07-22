ce344068cae569865c843cdfb3440b7822f7c4bb
canyon289 <ravinsdrive@gmail.com>
2014-07-22 10:37:07 -0500

    Starting assignment 2

---
+++ Exploratory Data Analysis/Assignment 2/Assignment2.r
@@ -0,0 +1,6 @@
+setwd("C:/Users/carbon/Dropbox/Github/DataScience/Exploratory Data Analysis/Assignment 2")
+
+#Read in data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
